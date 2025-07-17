30c42f028c06fa7119461a92ba28ca071c0f6426
Gift Amadi <amadigiftchukwudi@gmail.com>
2025-07-17 17:22:01 +0100

    tools commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // File: src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ import './index.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <main className="flex-grow">
         <Routes>
@@ -27,7 +27,7 @@ function App() {
         </Routes>
       </main>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
